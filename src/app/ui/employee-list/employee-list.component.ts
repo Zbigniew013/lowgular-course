119d9683daf13f54b920e0270261f433510b94ca
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-01-02 12:08:12 +0100

    lowgular course entry master

---
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -0,0 +1,17 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { EmployeeModel } from '../../model/employee.model';
+
+@Component({
+  selector: 'employee-list',
+  templateUrl: './employee-list.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmployeeListComponent {
+  data$: Observable<EmployeeModel[] | null> = of(null);
+}
